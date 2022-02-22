e1a1c36284fbe3b2b64d0d18d5465f952bdb68aa
shubham <shubhamkhatri767@gmail.com>
2022-02-22 16:51:13 +0530

    Github is done

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -18,6 +18,95 @@ class homepage extends StatelessWidget {
     );
   }
 
+  Widget stores(Size size) {
+    return Container(
+      height: size.height,
+      width: size.width,
+      child: ListView.builder(
+          itemCount: restaurantList.length,
+          itemBuilder: (context, index) {
+            return itemBuilder(size, index, context);
+          }),
+    );
+  }
+
+  Widget itemBuilder(Size size, int index, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 18.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: Material(
+          elevation: 3,
+          borderRadius: BorderRadius.circular(18),
+          child: Container(
+            height: size.height / 2.5,
+            width: size.width / 1.1,
+            child: Column(
+              children: [
+                Container(
+                  height: size.height / 4,
+                  width: size.width / 1.1,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(18),
+                      topRight: Radius.circular(18),
+                    ),
+                    image: DecorationImage(
+                        image: NetworkImage(restaurantList[index].imageUrl),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                Container(
+                  height: size.height / 12,
+                  width: size.width / 1.2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        restaurantList[index].title,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Container(
+                        height: size.height / 25,
+                        width: size.width / 7,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.green,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          restaurantList[index].rating,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: size.width / 1.2,
+                  child: Text(
+                    "${restaurantList[index].locations}   \t\t\t\t\t\t\t\t\t\t\t\t\t\t  ${restaurantList[index].price} for one",
+                    style: TextStyle(
+                      fontSize: 12.9,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget shops() {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 5),
@@ -55,6 +144,8 @@ class homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -119,36 +210,37 @@ class homepage extends StatelessWidget {
               ),
             ),
             SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                      child: Column(
-                        children: [items(), items()],
-                      ),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [items(), items()],
                     ),
-                    Container(
-                      child: Column(
-                        children: [items(), items()],
-                      ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [items(), items()],
                     ),
-                    Container(
-                      child: Column(
-                        children: [items(), items()],
-                      ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [items(), items()],
                     ),
-                    Container(
-                      child: Column(
-                        children: [items(), items()],
-                      ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [items(), items()],
                     ),
-                    Container(
-                      child: Column(
-                        children: [items(), items()],
-                      ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [items(), items()],
                     ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+            ),
             divider(),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10),
@@ -225,6 +317,7 @@ class homepage extends StatelessWidget {
                 ],
               ),
             ),
+            stores(size),
           ],
         ),
       ),
